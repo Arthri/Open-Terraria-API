2965c710caa566950792be9e90a40c2a81dd0231
Arthri <41360489+a@users.noreply.github.com>
2024-04-23 09:49:41 +0000

    Cleanup MonoMod

--- ModFramework/ModFramework/Modder.cs
+++ ModFramework/ModFramework/Modder.cs
@@ -91,6 +91,7 @@ public class ModFwModder : MonoMod.MonoModder, IRelinkProvider
 
     public ModFwModder(ModContext context)
     {
+        Environment.SetEnvironmentVariable("MONOMOD_CLEANUP_ALL", "1");
         ModContext = context;
         MethodParser = (MonoModder modder, MethodBody body, Instruction instr, ref int instri) => true;
         MethodRewriter = (MonoModder modder, MethodDefinition method) =>
